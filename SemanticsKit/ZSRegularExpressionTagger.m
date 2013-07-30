47f4f229b584f04ff9ee5c3b45fcbf0ccc126691
Steven Zhang <shsteven1000@GMail.com>
2013-07-30 23:18:57 +0800

    TODO: work out delta updating tags and concurrency issue

--- SemanticsKit/ZSRegularExpressionTagger.m
+++ SemanticsKit/ZSRegularExpressionTagger.m
@@ -15,6 +15,7 @@
 @end
 
 @implementation ZSRegularExpressionTagger
+@synthesize tags;
 
 - (id)initWithPattern: (NSString *)pattern
        operationQueue: (NSOperationQueue *)operationQueue
@@ -47,19 +48,21 @@
     return self;
 }
 
-- (NSArray *)tags {
-    return tags;
+- (void)generateTagsWithCompletion:(TaggerCompletionBlock)completion {
+    [self generateTagsForRange:NSMakeRange(0, self.textStorage.string.length)
+                withCompletion:completion];
 }
 
-- (void)generateTagsWithCompletion:(TaggerCompletionBlock)completion {
-    __weak typeof(self)weakSelf = self;;
+- (void)generateTagsForRange: (NSRange)range
+              withCompletion:(TaggerCompletionBlock)completion {
+    __weak typeof(self)weakSelf = self;
     
     NSBlockOperation *op = [NSBlockOperation blockOperationWithBlock:^{
         [tags removeAllObjects];
         
         [self.regularExpression enumerateMatchesInString:self.textStorage.string
                                                  options:0
-                                                   range:NSMakeRange(0, self.textStorage.string.length)
+                                                   range:range
                                               usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
                                                   ZSSemanticsTag *newTag = [ZSSemanticsTag new];
                                                   newTag.range = result.range;
@@ -69,13 +72,14 @@
                                                   [tags addObject:newTag];
                                               }];
 
+        weakSelf.taggingOperation = nil;
+        
     }];
     
     op.completionBlock = ^{
         dispatch_sync(dispatch_get_main_queue(), ^{
             if (completion)
                 completion(weakSelf);
-            weakSelf.taggingOperation = nil;
         });
 
     };
@@ -89,7 +93,10 @@
 }
 
 
+
 #pragma mark -
+
+// delta = newCharRange.length - oldCharRange.length. >0: insert       < 0: delete
 
 - (void)processEditingForTextStorage:(NSTextStorage *)textStorage
                               edited:(NSTextStorageEditActions)editMask
@@ -100,20 +107,78 @@
 #warning TODO: apply delta to all existing tags that are still valid
 //#warning TODO: tie into textStorage's attribute and piggy back on attributeAtIndex? looks unnecessary.
     
+    if (self.taggingOperation)
+        return;
+    
     if (editMask & NSTextStorageEditedCharacters) {
 //        NSLog(@"edite range: (%d, %d) changeInLength: %d", newCharRange.location, newCharRange.length, delta);
         
-        // Deduce invalidate range based on invalidatedCharRange
-        // Invalidate all tags in that range
-        // Rescan that range
-        // Sort tags by range.location
+#warning TODO: solve concurrency issue with getTagsAtIndex:
+        // 1. Deduce invalidate range based on invalidatedCharRange
+        NSRange rangeToRescan = [textStorage.string lineRangeForRange:invalidatedCharRange];
+
+        // Adjust other tags
+        [self adjustTagsAfterRange:rangeToRescan delta:delta];
         
+        // Invalidate all tags in that range
+        [self invalidateTagsInRange:rangeToRescan];
+        
+        // Rescan that range
+        __weak typeof(self)weakSelf = self;
+        [self generateTagsForRange:rangeToRescan withCompletion:^(id tagger) {
+            // Sort tags by range.location
+            [weakSelf sortTags];
+        }];
+        
+
+        /*
+        // DEBUG
+        if (self.type == kMentionTagType)
+            NSLog(@"delta: %d invalid: %@ new: %@", delta, NSStringFromRange(invalidatedCharRange), NSStringFromRange(newCharRange));
+         */
+        
+        /*
         [self generateTagsWithCompletion:^(id tagger) {
 #ifdef DEBUG
 //            NSLog(@"tags: %@", [tagger tags]);
 #endif
         }];
+         */
     }
+}
+
+#pragma mark -
+
+- (void)invalidateTagsInRange: (NSRange)range {
+    NSMutableArray *invalidatedTags = [NSMutableArray new];
+    [self.tags enumerateObjectsWithOptions:NSEnumerationConcurrent
+                                usingBlock:^(ZSSemanticsTag *aTag, NSUInteger idx, BOOL *stop) {
+                                    if (NSIntersectionRange(aTag.range, range).length) {
+                                        [invalidatedTags addObject:aTag];
+                                    }
+                                }];
+    
+    [tags removeObjectsInArray:invalidatedTags];
+}
+
+- (void)adjustTagsAfterRange: (NSRange)invalidatedRange
+                       delta: (NSInteger)delta {
+    [self.tags enumerateObjectsWithOptions:NSEnumerationConcurrent
+                                usingBlock:^(ZSSemanticsTag *aTag, NSUInteger idx, BOOL *stop) {
+                                    if (aTag.range.location >= NSMaxRange(invalidatedRange)) {
+                                        NSRange newRange = aTag.range;
+                                        newRange.location += delta;
+                                        aTag.range = newRange;
+                                    }
+                                }];
+}
+
+- (void)sortTags {
+    [tags sortUsingComparator:^NSComparisonResult(ZSSemanticsTag *tag1, ZSSemanticsTag *tag2) {
+        if (tag1.range.location < tag2.range.location) return NSOrderedAscending;
+        if (tag1.range.location > tag2.range.location) return NSOrderedDescending;
+        return NSOrderedSame;
+    }];
 }
 
 @end
